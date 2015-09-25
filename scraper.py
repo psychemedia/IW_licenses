4aff246c4e86de35baf32bc25a7e49a8fd1ee741
Tony Hirst <tony.hirst@gmail.com>
2015-09-25 08:53:30 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -112,6 +112,7 @@ def licenseScraper(ltype='Premises'):
 		if len(scraperwiki.sqlite.select("* from {t} where number={n}".format(t=t,n=r['number']))==0:
 			print('First seen',r)
 			r['firstSeen']=datetime.datetime.utcnow()
+			newRecords.append(r)
 	if len(newRecords):
 		print('Adding {} new records'.format(len(newRecords))
 		scraperwiki.sqlite.save(unique_keys=['number'],table_name=t, data=newRecords)
